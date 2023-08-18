9ec2df3e06e719de967a468450d16e4c348ac204
LakshyaGarg26 <lakshya0418.be20@chitkara.edu.in>
2023-08-18 17:51:38 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>TechyStar</h1>
+            <p>@all rights reserved</p>
+        </div>
+        <div>
+            <h5>Follow Us</h5>
+            <div>
+                <a href="https://www.youtube.com/">YouTube</a>
+                <a href="https://www.instagram.com/lakshya__26/">Instagram</a>
+                <a href="https://github.com/LakshyaGarg26">GitHub</a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
